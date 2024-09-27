3d478889fe1f0426ce1c819d804a1a325b0f24f9
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-09-27 20:57:07 +0300

    [refactor]: added @Param for native query

--- src/main/java/ru/kenpxrk/project/repository/CarRepository.java
+++ src/main/java/ru/kenpxrk/project/repository/CarRepository.java
@@ -2,14 +2,14 @@ package ru.kenpxrk.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.kenpxrk.project.model.Car;
 
 import java.util.List;
 
-
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-    @Query(value = "SELECT * FROM cars LIMIT ?1", nativeQuery = true)
-    List<Car> findAll(int count);
+    @Query(value = "SELECT * FROM cars LIMIT :count", nativeQuery = true)
+    List<Car> findAll(@Param("count") int count);
 }
