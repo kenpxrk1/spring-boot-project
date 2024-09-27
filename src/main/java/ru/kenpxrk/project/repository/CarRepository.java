3bc54338b88ff8787258f0224790cef30dce29e6
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-09-27 20:51:50 +0300

    [refactor]: rewrite findAll query to native sql

--- src/main/java/ru/kenpxrk/project/repository/CarRepository.java
+++ src/main/java/ru/kenpxrk/project/repository/CarRepository.java
@@ -1,11 +1,15 @@
 package ru.kenpxrk.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import ru.kenpxrk.project.model.Car;
+
+import java.util.List;
 
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
-
+    @Query(value = "SELECT * FROM cars LIMIT ?1", nativeQuery = true)
+    List<Car> findAll(int count);
 }
