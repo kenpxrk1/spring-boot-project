ea49f3e5bf104dbbabb8c2c8a34102308152e0f8
Dmitry Grishin <kenpxrkasf@gmail.com>
2024-09-29 21:21:03 +0300

    [refactor] delete unused code

--- src/main/java/ru/kenpxrk/project/client/IncomeData.java
+++ src/main/java/ru/kenpxrk/project/client/IncomeData.java
@@ -11,14 +11,7 @@ import java.io.Serializable;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class IncomeData implements Serializable {
     private int id;
     private int income;
-
-    @JsonCreator
-    public IncomeData(@JsonProperty("id") int id, @JsonProperty("income") int income) {
-        this.id = id;
-        this.income = income;
-    }
 }
